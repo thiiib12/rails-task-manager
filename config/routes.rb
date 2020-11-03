d98fb3a8001f2b15d99952c7396126a37ca29eee
thibault alexandre <thibault.a@hotmail.com>
2020-11-03 12:29:32 +0100

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'tasks' => 'tasks#index'
+  # get 'tasks/:id' => 'tasks#show'
+  # get 'tasks/new' => 'tasks#new'
+  # post 'tasks' => 'tasks#create'
+
+  resources :tasks
 end
